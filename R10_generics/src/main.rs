dd55530175b67dd2780c7bc55e017f126e9f1669
Pierre Violent <PierreViolent@eurofins.com>
2018-11-14 08:20:46 +0100

    More trait examples

--- R10_generics/src/main.rs
+++ R10_generics/src/main.rs
@@ -16,11 +16,13 @@ impl<T, U> Point<T, U> {
     }
 
     // This implementation moves value out of self and other...
-    fn mixup<V, W>(self, other: Point<V,W>) -> Point<T, W> {
-        Point {x: self.x, y:other.y}
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
-
 
 fn main() {
     let p1 = Point { x: 1, y: 2 };
@@ -29,21 +31,42 @@ fn main() {
     let p3 = p1.mixup(p2);
     println!("p3: ({}, {})", p3.x, p3.y);
 
-    let a1 = [1,4,3,12,3,5];
+    let a1 = [1, 4, 3, 12, 3, 5];
     let a2 = [3.1416, 2.7182, 1.4142, 1.7321];
     let a3 = ["once", "upon", "a", "time"];
-    println!("largest(a1): {}", largest(&a1));
-    println!("largest(a2): {}", largest(&a2));
-    println!("largest(a3): {}", largest(&a3));
+    println!("largest(a1): {}", largest1(&a1));
+    println!("largest(a2): {}", largest1(&a2));
+    println!("largest(a3): {}", largest1(&a3));
 }
 
-
-// Wait until I know how to add a trait constraint to a generic function
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+// Version using references
+fn largest1<T: PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
     for item in list.iter() {
         if item > largest {
             largest = item;
+        }
+    }
+    largest
+}
+
+// Version using Copy trait (value types)
+fn largest2<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+// Version 3 using Clone trait
+fn largest3<T: PartialOrd + Clone>(list: &[T]) -> T {
+    let mut largest = list[0].clone();
+    for item in list {
+        if *item > largest {
+            largest = item.clone();
         }
     }
     largest
