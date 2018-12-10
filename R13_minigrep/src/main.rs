352f7ae560ab6b1a31c7908995639f83b68ae3f2
FrenchBear38 FR03LAP00995 <FrenchBear38@outlook.com>
2018-12-10 08:27:27 +0100

    Progress on minigrep

--- R13_minigrep/src/main.rs
+++ R13_minigrep/src/main.rs
@@ -5,23 +5,40 @@
 
 use std::env;
 use std::fs;
-use std::io::prelude::*;
+use std::process;
+//use std::io::prelude::*;
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len()!=3 {
-        panic!("Usage: minigrep string file");
-    }
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {}", &config.query);
+    println!("In file {}", &config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect(&format!("Problem reading file {}", filename)[..]);
+    let contents = fs::read_to_string(&config.filename).expect(&format!("Problem reading file {}", &config.filename));
     println!("File contents:\n{}", contents);
 
 }
