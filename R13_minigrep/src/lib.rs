c77a98dc612f20fe58f580e7b3356523145e7314
French Bear <FrenchBear38@outlook.com>
2018-12-10 21:48:29 +0100

    before 2018 upgrade

---
+++ R13_minigrep/src/lib.rs
@@ -0,0 +1,31 @@
+// R13_minigrep, a rust version of grep
+// Learning rust
+// 2018-12-10	PV
+
+use std::error::Error;
+use std::fs;
+//use std::io::prelude::*;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.filename)?;
+    println!("File contents:\n{}", contents);
+    Ok(())
+}
--- R13_minigrep/src/main.rs
+++ R13_minigrep/src/main.rs
@@ -2,34 +2,14 @@
 // Learning rust
 // 2018-11-30	PV
 
+extern crate minigrep;
 
 use std::env;
-use std::fs;
 use std::process;
-//use std::io::prelude::*;
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
@@ -38,7 +18,8 @@ fn main() {
     println!("Searching for {}", &config.query);
     println!("In file {}", &config.filename);
 
-    let contents = fs::read_to_string(&config.filename).expect(&format!("Problem reading file {}", &config.filename));
-    println!("File contents:\n{}", contents);
-
+    if let Err(e) = minigrep::run(&config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
