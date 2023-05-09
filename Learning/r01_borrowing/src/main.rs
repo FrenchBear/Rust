4b98ede92ef5dc3c45a70bcb8887f0727039642f
French Bear <FrenchBear38@outlook.com>
2023-05-10 00:31:48 +0200

    Adder Learning folder to Rust

---
+++ Learning/r01_borrowing/src/main.rs
@@ -0,0 +1,18 @@
+// Just testing borrowing
+// 2023-05-08   PV
+
+fn main() {
+    let s = String::from("Hello, world");
+    let l = get_length(&s);
+    println!("len={l}");
+
+    let mut w = String::from("Bonjour");
+    let rw = &mut w;
+    rw.push_str(", ");
+    w.push_str("Pierre");
+    println!("{w}");
+}
+
+fn get_length(s: &String) -> usize {
+    s.len()
+}
