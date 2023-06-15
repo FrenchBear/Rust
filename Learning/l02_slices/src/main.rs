ddfc46e0c7269a75e5258180ec06dbf5776054da
French Bear <FrenchBear38@outlook.com>
2023-06-15 23:59:51 +0200

    save

--- Learning/l02_slices/src/main.rs
+++ Learning/l02_slices/src/main.rs
@@ -3,7 +3,14 @@
 //
 // 2023-05-16   PV
 
+#![allow(dead_code, unused_variables)]
+
 fn main() {
+    //test_slices();
+    test_loops();
+}
+
+fn test_slices() {
     let s = String::from("Once_upon_a_time");
     let b = first_space(&s);
     println!("First space: {b}");
@@ -43,4 +50,40 @@ fn first_word(s: &String) -> &str {
 
     let s = "Hello world";
     return &s[0..5];
+}
+
+fn test_loops() {
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    // iter - This borrows each element of the collection through each iteration. Thus leaving the collection untouched and available for reuse after the loop.
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            // TODO ^ Try deleting the & and matching just "Ferris"
+            _ => println!("Hello {}", name),
+        }
+    }
+    println!("names: {:?}", names);
+
+    // into_iter - This consumes the collection so that on each iteration the exact data is provided. Once the collection has been consumed it is no longer available for reuse as it has been 'moved' within the loop.
+    let names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+    //println!("names: {:?}", names);
+    // FIXME ^ Comment out this line
+
+    // iter_mut - This mutably borrows each element of the collection, allowing for the collection to be modified in place.
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+    println!("names: {:?}", names);
+
 }
--- Learning/l08_collections/src/main.rs
+++ Learning/l08_collections/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     arrays();
     my_list();
     vectors();
+    strings();
 }
 
 fn arrays() {
@@ -42,14 +43,12 @@ fn my_list() {
 }
 
 struct List {
-    v:Vec<i32>,
+    v: Vec<i32>,
 }
 
 impl List {
     fn new() -> List {
-        List {
-            v: Vec::new()
-        }
+        List { v: Vec::new() }
     }
 
     fn add(&mut self, value: i32) {
@@ -102,15 +101,15 @@ fn vectors() {
     // Sort vector
     v2.sort();
 
-    v2.sort_by(|a, b| b.cmp(a));    // Reverse order    https://stackoverflow.com/questions/60916194/how-to-sort-a-vector-in-descending-order-in-rust
+    v2.sort_by(|a, b| b.cmp(a)); // Reverse order    https://stackoverflow.com/questions/60916194/how-to-sort-a-vector-in-descending-order-in-rust
 
     use std::cmp::Reverse;
-    v2.sort_by_key(|w| Reverse(*w));    // Reverse order too
+    v2.sort_by_key(|w| Reverse(*w)); // Reverse order too
 
-    v2.sort();  // Sort then reverse, but not always equivalent to reverse sort using keys comparison, and not stable
+    v2.sort(); // Sort then reverse, but not always equivalent to reverse sort using keys comparison, and not stable
     v2.reverse();
 
-    v2.remove(1);   // Delete element using index
+    v2.remove(1); // Delete element using index
 
     // References in Rust are handled differently than references in C++ or C#
     let mut n = 5;
@@ -144,5 +143,72 @@ fn vectors() {
     vv.push(Vec::new());
     vv.push(Vec::new());
     vv.push(Vec::new());
+}
+
+fn strings() {
+    println!();
+
+    let mut s = String::from("foo");
+    s.push_str("bar"); // push_str use a str& and does not take ownership of parameter
+    s.push('!'); // add single char
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+                       // Although let s3 = s1 + &s2; looks like it will copy both strings and create a new one, this statement actually takes ownership of s1, appends a copy of the contents of s2, and then returns ownership of the result.
+                       // Signature of + operator: (it's self, not &self):  fn add(self, s: &str) -> String {
+
+    let s4 = s2.clone() + &s3;
+    //println!("s1={s1}");  // s1 not valid
+    println!("s2={s2}");
+    println!("s3={s3}");
+    println!("s4={s4}");
+
+    // Format works like println! and does not take ownership of parameters
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{s1}-{s2}-{s3}"); // tic-tac-toe
+
+    // -----------
+    println!();
+    let s = "Aéà♫山𝄞🐗"; // à is decomposed form (combining accent and a)
+    println!("{}", s);
+
+    let bs = s.as_bytes();
+    let mut i=0;
+    while i<s.len() {
+        let b = bs[i];
+        if b<128 {
+            if let Ok(c) = String::from_utf8(bs[i..=i].to_vec()) {
+                print!("[{b:02x}: {c}]");
+            }
+            i+=1;
+        } else if b&0b11100000 == 0b11000000 {
+            let b2 = bs[i+1];
+            if let Ok(c) = String::from_utf8(bs[i..=i+1].to_vec()) {
+                print!("[{b:02x} {b2:02x}: {c}]");
+            }
+            i+=2;
+        } else if b&0b11110000 == 0b11100000 {
+            let b2 = bs[i+1];
+            let b3 = bs[i+2];
+            if let Ok(c) = String::from_utf8(bs[i..=i+2].to_vec()) {
+                print!("[{b:02x} {b2:02x} {b3:02x}: {c}]");
+            }
+            i+=3;
+        } else if b&0b11111000 == 0b11110000 {
+            let b2 = bs[i+1];
+            let b3 = bs[i+2];
+            let b4 = bs[i+3];
+            if let Ok(c) = String::from_utf8(bs[i..=i+3].to_vec()) {
+                print!("[{b:02x} {b2:02x} {b3:02x} {b4:02x}: {c}]");
+            }
+            i+=4;
+        } else {
+            panic!("Error");
+        }
+    }
+    println!();
     
 }
