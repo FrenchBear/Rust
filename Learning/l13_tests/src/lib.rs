2f2ee0a97fdf630af5310655084db369a2fac73e
French Bear <FrenchBear38@outlook.com>
2023-06-24 00:45:46 +0200

    save

--- Learning/l13_tests/src/lib.rs
+++ Learning/l13_tests/src/lib.rs
@@ -1,6 +1,12 @@
 // l12_tests
 // Learning Rust again
 //
+// Use cargo test --help to show the options you can use with cargo test.
+// When running tests (cargo test), output is not displayed by default, or use «cargo test -- --show-output»
+// By default, tests run in parallel, you must make sure your tests don’t depend on each other or on any shared state, including a shared environment,
+// such as the current working directory or environment variables. Or use «cargo test -- --test-threads=1» to disable parallelism.
+// Use «cargo test -- --ignored» to run tests with #[ignore] attribute (ex: expensive tests)
+//
 // 2023-06-23   PV
 
 #![allow(unused)]
@@ -26,6 +32,10 @@ pub fn greeting(name: &str) -> String {
     format!("Hello")
 }
 
+// The attribute cfg stands for configuration and tells Rust that the following item should only be included given a certain configuration option.
+// In this case, the configuration option is test, which is provided by Rust for compiling and running tests. By using the cfg attribute,
+// Cargo compiles our test code only if we actively run the tests with cargo test. This includes any helper functions that might be within this module,
+// in addition to the functions annotated with #[test].
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -75,5 +85,49 @@ mod tests {
             "Greeting did not contain name, value was `{}`",
             result
         );
+    }
+}
+
+// -----------------------
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests2 {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected="Guess value must be between 1 and 100, got 200.")] // Can optionally use expected="msg" to be sure that the function parics for the correct reason
+    fn greater_than_100() {
+        let g = Guess::new(200);
+    }
+}
+
+// -----------------------
+
+#[cfg(test)]
+mod tests3 {
+    use super::*;
+
+    // A test function that has the Result<T, E> will pass if it returns T, and fails if it returns E.
+    // Using operator ? is a convenient way to to fail the test if any operation within returns an Err variant.
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if add(2,2)==4 {
+            Ok(())
+        } else {
+            Err(String::from("Adding 2 and 2 doesn't make 4!"))
+        }
     }
 }
