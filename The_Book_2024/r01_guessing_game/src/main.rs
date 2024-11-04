e9c3ab801d09c26c49dbc556b1113e7042f086ac
French Bear <FrenchBear38@outlook.com>
2024-11-04 23:12:00 +0100

    Restart learning rust

---
+++ The_Book_2024/r01_guessing_game/src/main.rs
@@ -0,0 +1,44 @@
+// r01_guessing_game
+// Learning rust
+//
+// 2024-11-02   PV      Re-restart Rust learning
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Devinez le nombre!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // For dev/debug only!
+    //println!("Le nombre secret est: {secret_number}");
+
+    loop {
+        println!("Entrez votre essai");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Erreur de lecture");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Entrez une valeur numérique! entre 1 et 100!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Trop petit!"),
+            Ordering::Greater => println!("Trop grand!"),
+            Ordering::Equal => {
+                println!("Gagné!");
+                break;
+            }
+        }
+    }
+}
---
+++ The_Book_2024/r02_integer_overflow/src/main.rs
@@ -0,0 +1,22 @@
+// r02_integer_overflow
+// Test functions related to integer overflow
+//
+// 2024-11-04   PV
+
+fn main() {
+    let a:u8 = 150;
+    let b:u8 = 150;
+    // let c:u8 = a+b;      // Doesn't even compile, causes attempt to compute `150_u8 + 150_u8`, which would overflow
+
+    let c1 = u8::wrapping_add(a, b);    // Returns (a + b) mod 2^N, where N is the width of T in bits
+    println!("wrapping_add: c1={c1}");
+
+    let c2 = u8::checked_add(a, b);
+    match c2 {
+        Some(res) => println!("checked_add: Ok, c2={res}"),
+        None => println!("checked_add: Overflow"),
+    };
+
+    let c3=u8::saturating_add(a, b);    // Saturating at the numeric bounds instead of overflowing
+    println!("saturating_add: c3={c3}");
+}
