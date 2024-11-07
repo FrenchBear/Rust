3ddb7943605d98fd698fc8415362a0821c415fe6
French Bear <FrenchBear38@outlook.com>
2024-11-07 21:22:51 +0100

    More rust

---
+++ The_Book_2024/r07_enums/src/main.rs
@@ -0,0 +1,83 @@
+// r07_enums
+// Learning rust 2024
+//
+// 2024-11-07   PV
+
+#![allow(dead_code, unused_variables)]
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+// Enum with fourr variants of different types, grouped under the Message type
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// Equivalent to define 4 struct definitions, but we can't easily write a function taking any of these 4 messages:
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+
+// Can define methods on enums:
+impl Message {
+    fn call(&self) {
+        // Method body here
+    }
+}
+
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let home = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr2::V6(String::from("::1"));
+
+    let home = IpAddr3::V4(127, 0, 0, 1);
+    let loopback = IpAddr3::V6(String::from("::1"));
+
+    // Example calling a method of a Message
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+}
