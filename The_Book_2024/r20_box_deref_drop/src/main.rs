d010aa8fc77b1b73005dac92e3ea94858ab00fd8
French Bear <FrenchBear38@outlook.com>
2025-01-10 16:23:38 +0100

    More learning Rust

--- The_Book_2024/r20_box_deref_drop/src/main.rs
+++ The_Book_2024/r20_box_deref_drop/src/main.rs
@@ -1,16 +1,23 @@
-// r20_smart_pointers
-// Learning rust 2024
+// r20_box_deref_drop
+// Learning rust 2024, Smart Pointers 1
 //
 // 2025-01-09   PV
 
 #![allow(dead_code, unused_variables)]
 
+use std::ops::Deref;
 use std::time::Instant;
 
 // A recursive type, without box, compiler wouldn't know the size of List
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        println!("drop called on struct List instance");
+    }
 }
 
 // Variant using a ref
@@ -33,11 +40,25 @@ fn main() {
     // Note that a ref also works
     let list2 = Cons2(1, &Cons2(2, &Cons2(3, &Nil2)));
 
+    // Force early from of variable
+    drop(list);
+
     // Contrary to what is repeated in the Book, of course, boxing/unboxing has a penaly cost
     // And contrary to .Net where boxing/unboxing is invisible, in Rust, unboxing is explicit with dereferencement operator *
     test_box_performace();
 
+    let x = MyBox::new(5);
+    let y = 5;
+    assert_eq!(*x, y);
 
+    let n1 = MyBox::new("Pierre");
+    hello(&n1); // Deref coercion, calling hello with &MyBox<&str>, converted automatically to &str
+
+    let n2 = MyBox::new(String::from("world"));
+    hello(&n2);
+    // Two Deref coercions chained to go from &MyBox<String> to &str: First converted to &String calling deref,
+    // then standard library Deref on String, converting it into a &str. This is equivalent to:
+    hello(&(*n2)[..]);
 }
 
 fn test_box_performace() {
@@ -65,4 +86,25 @@ fn test_box_performace() {
     let duration = start.elapsed().as_millis();
     println!("test heap: {duration}ms");
     // 1231 ms
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
+// Simple implementation similar to Box
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
---
+++ The_Book_2024/r21_rc/src/main.rs
@@ -0,0 +1,37 @@
+// r21_rc
+// Learning rust 2024, Smart Pointers 2
+//
+// 2025-01-10   PV
+
+#![allow(dead_code, unused_variables)]
+
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        println!("drop called on struct List instance");
+    }
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    // Rc<T>, reference counted
+    // Can create clones, content is dropped once last clone is dropped
+    // Allow only immutable borrows
+    let ra = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&ra));
+    {
+        let c = Cons(4, Rc::clone(&ra));
+        println!("a={:?} ref count={}", ra, Rc::strong_count(&ra));
+    }
+    println!("a={:?} ref count={}", ra, Rc::strong_count(&ra));
+
+
+}
