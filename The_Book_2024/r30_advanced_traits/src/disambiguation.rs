4815005430ed45249fc54695eec94f5b429671fe
French Bear <FrenchBear38@outlook.com>
2025-02-18 12:44:23 +0100

    More learning Rust

---
+++ The_Book_2024/r30_advanced_traits/src/disambiguation.rs
@@ -0,0 +1,75 @@
+// Advanced traits, disambiguation
+//
+// 2025-02-18   PV
+
+
+// Calling Methods (=having a &self parameter) with the same name
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+pub fn disambiguation_methods() {
+    let person = Human;
+
+    person.fly();   // Call method implemented on Human directly (prints *waving arms furiously*)
+    // Specifying trait name before method to select specific version of fly()
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+}
+
+
+// calling associated funtions of the same name
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
+pub fn disambiguation_associated_functions() {
+    println!("A baby dog is called a {}", Dog::baby_name());        // Calls the function defined on Dog directly (Spot)
+    //println!("A baby dog is called a {}", Animal::baby_name());   // cannot call associated function on trait without specifying the corresponding `impl` type
+
+    // To disambiguate and tell Rust that we want to use the implementation of Animal for Dog as opposed to the implementation of Animal
+    // for some other type, we need to use fully qualified syntax. 
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+
+    // fully qualified syntax is defined as follows:
+    // <Type as Trait>::function(receiver_if_method, next_arg, ...);
+
+}
---
+++ The_Book_2024/r30_advanced_traits/src/main.rs
@@ -0,0 +1,60 @@
+// r30_advanced_traits
+// Learning rust 2024, Advanced features
+//
+// 2025-02-18   PV
+
+#![allow(unused, static_mut_refs)]
+
+use std::ops::Add;
+
+mod disambiguation;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// Implementing the Add trait to overload + operator for Point instances
+// Add trait has an associated type named Output that determines the type returned from the add method
+// Here is the definition of Add trait:
+//
+// trait Add<Rhs=Self> {
+//     type Output;
+//     fn add(self, rhs: Rhs) -> Self::Output;
+// }
+// Rhs defaults to Self if we don't specify a concrete type for Rhs when implementing Add, making it easier to implement
+
+impl Add for Point {
+    type Output = Point;        
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+// Example of implementation of Add specifying Rhs
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    disambiguation::disambiguation_methods();
+    disambiguation::disambiguation_associated_functions();
+}
