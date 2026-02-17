c5befe4d71b8c11d7a9a0bbb555ac9e8066a0060
French Bear <frenchbear38@outlook.com>
2026-02-18 00:38:38 +0100

    Icon and signature for runiq

--- RUtils/Runiq/build.rs
+++ RUtils/Runiq/build.rs
@@ -3,6 +3,7 @@
 //
 // 2025-07-05   PV      First version, with the help of Gemini
 // 2025-10-22   PV      Clippy review
+// 2026-02-18   PV      Added icon, file description and copyright
 
 use std::env;
 use std::fs;
@@ -19,26 +20,60 @@ fn main() {
     let lockfile: toml::Value = toml::from_str(&lock_content).expect("Failed to parse Cargo.lock");
 
     // Find the package entry
-    let packages = lockfile.get("package").and_then(|p| p.as_array()).expect("Could not find [[package]] in Cargo.lock");
+    let packages = lockfile
+        .get("package")
+        .and_then(|p| p.as_array())
+        .expect("Could not find [[package]] in Cargo.lock");
 
     generate_variable(packages, "getopt");
 
     // Tell cargo to re-run the build script if Cargo.lock changes.
     println!("cargo:rerun-if-changed=Cargo.lock");
+
+    // Retrieve copyright from cargo.toml section [package.metadata]
+    let manifest_path = env::var("CARGO_MANIFEST_DIR").unwrap() + "/Cargo.toml";
+    let metadata = cargo_metadata::MetadataCommand::new()
+        .manifest_path(manifest_path)
+        .no_deps() // We only care about our own crate
+        .exec()
+        .unwrap();
+
+    // Access the [package.metadata] JSON object
+    let root_package = metadata.root_package().unwrap();
+    let copyright = root_package.metadata["copyright"].as_str().unwrap_or_default();
+
+    // [package] info can be accessed directly
+    let description = env!("CARGO_PKG_DESCRIPTION");
+    let name = env!("CARGO_PKG_NAME");
+
+    // Include an icon
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("uniq.ico");
+        res.set("FileDescription", format!("{name}: {description}").as_str());
+        res.set("LegalCopyright", copyright);
+        res.compile().unwrap();
+    }
 }
 
 fn generate_variable(packages: &[Value], dependency_name: &'static str) {
-    let dep_package = packages.iter().find(|p| {
-        p.get("name").and_then(|n| n.as_str()) == Some(dependency_name)
-    }).unwrap_or_else(|| panic!("Could not find '{}' in Cargo.lock's [[package]] list", dependency_name));
+    let dep_package = packages
+        .iter()
+        .find(|p| p.get("name").and_then(|n| n.as_str()) == Some(dependency_name))
+        .unwrap_or_else(|| panic!("Could not find '{}' in Cargo.lock's [[package]] list", dependency_name));
 
     // Extract the version
-    let version = dep_package.get("version")
+    let version = dep_package
+        .get("version")
         .and_then(|v| v.as_str())
         .unwrap_or_else(|| panic!("Could not find version for '{}' in Cargo.lock", dependency_name));
 
     // Expose the version as a new environment variable
     // This `cargo:rustc-env` instruction tells Cargo to set the `DEP_XXXX_VERSION`
     // environment variable for the compilation of the main crate.
-    println!("cargo:rustc-env=DEP_{}_VERSION={}", dependency_name.to_uppercase().replace('-', "_"), version);
+    println!(
+        "cargo:rustc-env=DEP_{}_VERSION={}",
+        dependency_name.to_uppercase().replace('-', "_"),
+        version
+    );
 }
